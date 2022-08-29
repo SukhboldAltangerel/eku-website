df631d55bb861ef46b71b85ec9fc312f3db3dd31
Sukhbold Altangerel <col.de.staz@gmail.com>
2022-08-29 23:35:38 +0800

    .

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,7 @@ import styles from './home.module.css'
 import video from '../../assets/1609140086717.webm'
 import kappa from '../../assets/kappa.png'
 import song from '../../assets/song.mp3'
-import { MouseEventHandler } from 'react'
+import cat from '../../assets/cat.gif'
 
 export default function Home() {
    function stopPropagation(e: any) {
@@ -12,14 +12,20 @@ export default function Home() {
    return (
       <div className={styles.container}>
          <video className={styles.video} autoPlay muted loop src={video} />
+         <img className={styles.cat} src={cat} />
          <div className={styles.container1} onClick={stopPropagation}>
             <div className={styles.introContainer}>
-               Дуучин EKU-гийн вебсайтад тавтай морил :))
+               <span>
+                  <a href="https://www.instagram.com/cybertearzzz/">Дуучин EKU</a>-гийн вебсайтад тавтай морил :))
+               </span>
                <img className={styles.image} src={kappa} />
             </div>
             <div className={styles.songContainer}>
-               Та бүхэн үзээрийн эмо дууг нь сонсоорой
+               Та бүхэн дууг нь сонсоорой
                <audio src={song} controls />
+            </div>
+            <div className={styles.rain}>
+               I'll be fucked up in the rain
             </div>
             <div className={styles.rights}>
                All rights partially reserved Ⓒ 2022 by Erchis
